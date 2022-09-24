169a3950d839df0f1b858db6598169cddfb19798
Kubanych Asanov <kuba.asanov4@gmail.com>
2022-09-24 20:13:07 +0600

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,12 +1,1 @@
-import 'leetcode/88_merge_sorted_array.dart';
-
-void main(List<String> args) {
-  MergeSortedArray mergeSortedArray = MergeSortedArray();
-
-  // mergeSortedArray.merge2([4, 5, 6, 0, 0, 0], 3, [1, 2, 3], 3);
-  // mergeSortedArray.merge2([0], 0, [1], 1);
-  // mergeSortedArray.merge2([1, 0], 1, [2], 1);
-  // mergeSortedArray.merge2([0, 0, 0, 0, 0], 0, [1, 2, 3, 4, 5], 5);
-  mergeSortedArray.merge2([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3);
-  // mergeSortedArray.merge2([4, 0, 0, 0, 0, 0], 1, [1, 2, 3, 5, 6], 5);
-}
+void main(List<String> args) {}
